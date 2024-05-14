95236be72a44e101b9ecfa5bb5b07213e368720e
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-05-14 21:35:47 +0500

    Modifying a Global Variable in Python using global and return keyword

--- Local-and-Global-Scope-in-Python.py
+++ Local-and-Global-Scope-in-Python.py
@@ -9,7 +9,7 @@ def increase_enemies():
 
 
 increase_enemies()
-print(f"enemies outside function: <{enemies}>")
+print(f"enemies outside function: <{enemies}>\n")
 
 
 # Local Scope
@@ -29,4 +29,30 @@ def energy():
 
 
 energy()
-print(f"<Global Scope> allowed outside a function <{player_health}>")
+print(f"<Global Scope> allowed outside a function <{player_health}>\n")
+
+# Modifying a Global Variable way:1
+
+enemies1 = 33
+def increase_enemies1():
+    # global keyword is used to modify a global variable inside a function
+    global enemies1
+    enemies1 +=1
+    print(f"enemies1 inside a function: {enemies1}")
+
+increase_enemies1()
+print(f"enemies1 outside a function: {enemies1}")
+
+
+
+
+
+# Modifying a Global Variable way:2
+
+enemies2 = 10
+def increase_enemies2():
+    print(f"enemies2 inside a function: {enemies2}")
+    return enemies2 + 1
+
+enemies2 = increase_enemies2()
+print(f"enemies2 outside a function: {enemies2}")
