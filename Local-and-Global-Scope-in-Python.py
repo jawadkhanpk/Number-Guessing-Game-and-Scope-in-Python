3ca4702a011c9e3857728cedce2437959f25df29
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-05-14 21:17:08 +0500

    Namespace Local and Global Scope in Python

--- Local-and-Global-Scope-in-Python.py
+++ Local-and-Global-Scope-in-Python.py
@@ -1,1 +1,32 @@
-print("Hello World")
+################## Scope ##################
+
+enemies = 1
+
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: <{enemies}>")
+
+
+increase_enemies()
+print(f"enemies outside function: <{enemies}>")
+
+
+# Local Scope
+def drink_potion():
+    potion_strength = 2
+    print(f"<Local Scope> allowed only inside a function <{potion_strength}>")
+
+
+drink_potion()
+
+# Global Scope
+player_health = 20
+
+
+def energy():
+    print(f"<Global Scope> allowed inside a function <{player_health}>")
+
+
+energy()
+print(f"<Global Scope> allowed outside a function <{player_health}>")
